c79501b0a63265b360d906ee00ac19f6957207b7
mounirelbakkali1 <mounir/elbakkali1@etu.uae.ac.ma>
2023-01-02 23:38:48 +0100

    moved from data from json to retreiving data from mySQL

---
+++ Models/QuizController.php
@@ -0,0 +1,121 @@
+<?php
+require 'DBConnection.php';
+// if (isset($_GET['questions'])) getQuizQuestions();
+class QuizController extends DBConnection
+{
+
+    public function getQuizzes()
+    {
+        $ststm = $this->connect()->prepare("SELECT * FROM quizes");
+        $ststm->execute();
+        $quizes = $ststm->fetchAll();
+        $fullfilledArray = array();
+        foreach ($quizes as $quiz) {
+            $countQuestions = count($this->getQuestions($quiz['id']));
+            array_push($fullfilledArray, array("quiz" => $quiz, "numOfQuestions" => $countQuestions));
+        }
+        return $fullfilledArray;
+    }
+
+    public function getQuizQuestions($id_quiz)
+    {
+        $questions = $this->getQuestions($id_quiz);
+        $responses = $this->getResponses($id_quiz);
+        $new_array = array();
+        for ($i = 0; $i < count($questions); $i++) {
+            $sum_ofOptions = array();
+            for ($j = 0; $j < count($responses); $j++) {
+                if ($questions[$i]['id'] == $responses[$j]['question_id']) array_push($sum_ofOptions, $responses[$j]);
+            }
+            $new = array("id" => $questions[$i]['id'], "question" => $questions[$i]['question'], "options" => $sum_ofOptions);
+            array_push($new_array, $new);
+        }
+        return  $new_array;
+    }
+
+    public function compareAnswer($userAnswers, $id_quiz)
+    {
+        $correctAnswers = $this->getCorrection($id_quiz);
+        $correctOnes = 0;
+        for ($i = 0; $i < count($correctAnswers); $i++) {
+            $element = $correctAnswers[$i]['id'];
+            if ($element == $userAnswers[$i]['resId']) $correctOnes++;
+        }
+        $score = ($correctOnes / count($userAnswers)) * 100;
+        $feedBack = ($score < 30) ? "Not Bad" : (($score < 60) ? "Good results" : "Exellent keep up");
+        $template = $this->showFeedBack($userAnswers, $correctAnswers, $id_quiz);
+        return array("template" => $template, "feedback" => $feedBack, "score" => $score, "correctOnes" => $correctOnes);
+    }
+
+
+    private function getResponses($id_quiz)
+    {
+        $ststm = $this->connect()->prepare("SELECT responses.id ,responses.response as content,responses.question_id FROM responses INNER join questions on responses.question_id = questions.id where questions.quiz_id =? ;");
+        $ststm->execute(array($id_quiz));
+        return $ststm->fetchAll();
+    }
+
+    private function getCorrection($id_quiz)
+    {
+        $ststm = $this->connect()->prepare("SELECT responses.id ,responses.question_id,responses.comment FROM responses INNER join questions on responses.question_id = questions.id where questions.quiz_id =? and responses.isCorrect=1;");
+        $ststm->execute(array($id_quiz));
+        return $ststm->fetchAll();
+    }
+
+
+    private function getQuestions($quizID)
+    {
+        $ststm = $this->connect()->prepare("SELECT * from questions where quiz_id=?");
+        $ststm->execute(array($quizID));
+        return $ststm->fetchAll();
+    }
+    private function showFeedBack($userAnswers, $correctAnswers, $id_quiz)
+    {
+        $template = "";
+        $i = 0;
+        $questions = $this->getQuestions($id_quiz);
+        $optionsWIthqsts = $this->getQuizQuestions($id_quiz);
+        // print_r(json_encode($userAnswers));
+        // echo "<hr>";
+        // print_r(json_encode($correctAnswers));
+        // echo "<hr>";
+        // print_r(json_encode($optionsWIthqsts));
+
+        foreach ($questions as $question) {
+            // var_dump($correctAnswers[$i]['id']);
+            $optionsTmplate = "";
+            $_class = "";
+            $u = 1;
+            foreach ($optionsWIthqsts[$i]['options'] as $option) {
+                // var_dump($userAnswers[$i]['resId'] == $correctAnswers[$i]['id']);
+                // echo "<br>";
+                // var_dump($correctAnswers[$i]['id'] == $option['id']);
+                // echo "<hr>";
+
+                if (
+                    $userAnswers[$i]['resId'] == $correctAnswers[$i]['id'] &&
+                    $correctAnswers[$i]['id'] == $option['id']
+                ) {
+                    $_class = "success";
+                } else if (
+                    $userAnswers[$i]['resId'] != $correctAnswers[$i]['id'] &&
+                    $correctAnswers[$i]['id'] == $option['id']
+                )
+                    $_class = "wrong";
+                else $_class = "";
+                $optionsTmplate .= "<div class='option $_class'><div style='display:flex'><span>" . $u . "</span>" . $option['content'] . "</div></div>";
+                $u++;
+            };
+            $template .= "
+                <div>
+                <p>Question " . $question['id'] . "/" . count($questions) . "</p>
+                <h3 style='padding:30px 0px'>" . $question['question'] . "</h3>
+                $optionsTmplate
+                <p class='score_question_comment'><img src='../assets/img/icons8-ok-48.png'>" . $correctAnswers[$i]['comment'] . "</p>
+                </div>";
+            $i++;
+        };
+
+        return $template;
+    }
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,31 +5,46 @@ window.onbeforeunload = function (event) {
 $(document).ready(async function () {
   loadQuize();
 });
-let randomArr = random(10, 0);
+let randomArr;
 
 async function loadQuize() {
+  //$(".score").hide();
+  hideScore();
   document.getElementById("questionaire").classList.remove("active");
   document.getElementById("result_part").classList.remove("active");
-  let url = "http://localhost:3000/quiz";
-  let res = await fetch(url)
-    .then((data) => data.json())
-    // .then((data) => console.log(data.quiz))
-    .then((data) => afficher(data))
-    .catch(async () => {
-      let res = await fetch("../data/db.json")
-        .then((data) => data.json())
-        // .then((data) => console.log(data.quiz))
-        .then((data) => afficher(data.quiz));
-    });
+  // let url = "http://localhost:3000/quiz";
+  // let res = await fetch(url)
+  //   .then((data) => data.json())
+  //   .then((data) => afficher(data))
+  //   .catch(async () => {
+  //     let res = await fetch("../data/db.json")
+  //       .then((data) => data.json())
+  //       // .then((data) => console.log(data.quiz))
+  //       .then((data) => afficher(data.quiz));
+  //   });
+  var quizzes = $.ajax({
+    type: "GET",
+    url: "../Models/RequestHandler.php",
+    data: {
+      //questions: JSON.stringify(data),
+      loadQuizes: true,
+    },
+    //dataType: "json",
+    cache: false,
+    async: false,
+  });
+  quizzes = JSON.parse(quizzes.responseText);
+  afficher(quizzes);
   function afficher(data) {
     let output = "";
+    console.log(data);
     data.forEach((quiz) => {
       output += `
-		   <div class="quiz-box">
-		   <h3>${quiz.title}</h3>
-		   <p>${quiz.questions.length} questions</p>
-		   <p>${quiz.description}</p>
-		   <button class='btn btn-orange bold'onclick='startQuiz(${quiz.id})'>start quiz</button>
+		   <div class="quiz-box" style='margin-top:20px'>
+		   <h3>${quiz.quiz.title}</h3>
+		   <p>${quiz.numOfQuestions} questions</p>
+		   <p>${quiz.quiz.description}</p>
+		   <button class='btn btn-orange bold'onclick='startQuiz(${quiz.quiz.id},${quiz.numOfQuestions})'>start quiz</button>
 		   </div>
 		   `;
     });
@@ -38,7 +53,8 @@ async function loadQuize() {
   return;
 }
 
-function startQuiz(idQuiz) {
+function startQuiz(idQuiz, numOfQuestions) {
+  randomArr = random(numOfQuestions, 0);
   document.getElementById("questionaire").classList.add("active");
   document.getElementById("result_part").classList.remove("active");
   document.querySelector(".score").style.opacity = "0";
@@ -48,39 +64,41 @@ function startQuiz(idQuiz) {
     height: "+=200px",
     padding: "30px",
   });
-  let questions = fetch("http://localhost:3000/quiz/" + idQuiz)
-    .then((data) => data.json())
-    .then(() => {
-      throw new Error("jjeze");
-    })
-    .then((data) => diplayquestions(data))
-    .catch(() => {
-      let qts = fetch("../data/db.json")
-        .then((data) => data.json())
-        // .then((data) => console.log(data.quiz[idQuiz - 1]))
-        .then((data) => diplayquestions(data.quiz[idQuiz - 1]));
-    });
+  var questions = $.ajax({
+    type: "GET",
+    url: "../Models/RequestHandler.php",
+    data: { questions: true, quizId: idQuiz },
+    dataType: "json",
+    cache: false,
+    async: false,
+  });
+  questions = JSON.parse(questions.responseText);
+  // console.log(questions);
+  diplayquestions(questions, idQuiz);
 }
-function diplayquestions(quiz) {
+function diplayquestions(quiz, idQuiz) {
+  console.log(quiz);
+  console.log(idQuiz);
   function processQuestions(question) {
     let options = "";
     // The every() function behaves exactly like forEach(), except it stops iterating through the array whenever the callback function returns a falsy value.
-
+    let u = 1;
     question.options.forEach((option) => {
       options += `
           <div class="option" style="position:relative;">
             <div style="display:flex">
-              <span>${option.id}</span>
+              <span>${u}</span>
               <input type="radio" name="response" class="radio" value="${option.id}">
-              ${option.content}
+              <p style="margin:10px">${option.content}</p>
             </div>
           </div>`;
+      u++;
     });
 
     let template = `<div id="quiz-questions-box" style="position:relative">
         <div><i class="bi bi-box" style="margin-right:10px"></i>AWS - QUIZ</div>
         <div style="display:flex;justify-content:space-between;">
-        <p>Question ${index + 1}/${quiz.questions.length}</p>
+        <p>Question ${index + 1}/${quiz.length}</p>
         <p id="seconds"></p>
         </div>
         <h3 style="padding:0px">${question.question}</h3>
@@ -103,7 +121,8 @@ function diplayquestions(quiz) {
   }
   let index = 0; // The index of the next element to show
   function doNext() {
-    processQuestions(Object.values(quiz.questions)[randomArr[index]]); // affichage des questions
+    // console.log(quiz);
+    processQuestions(quiz[randomArr[index]]); // affichage des questions
     /*
 	
 	------------------------ TIMER ---------------
@@ -130,7 +149,7 @@ function diplayquestions(quiz) {
           resId: 0,
           quizId: form.quizeID.value,
         });
-        if (index == quiz.questions.length) showresult(quiz);
+        if (index == quiz.questions.length) showresult(quiz, idQuiz);
       }
     }, 1000);
     /*
@@ -159,8 +178,8 @@ function diplayquestions(quiz) {
       clearInterval(secondsCounter);
       secondsCounter = null;
       passed = true;
-      if (index == quiz.questions.length) {
-        showresult(quiz);
+      if (index == quiz.length) {
+        showresult(quiz, idQuiz);
         return;
       } else {
         clearInterval(timer);
@@ -180,7 +199,7 @@ function diplayquestions(quiz) {
         false
       );
     });
-    if (index < Object.values(quiz.questions).length) {
+    if (index < quiz.length) {
       var timer = setTimeout(doNext, 30000); // 5000 for a five-second delay
     }
   }
@@ -188,69 +207,41 @@ function diplayquestions(quiz) {
   var secondsCounter;
   doNext();
 }
-function showresult(data) {
+function showresult(data, idQuiz) {
+  // alert("done");
   document.getElementById("result_part").classList.add("active");
   let arr = holderUserAnswersArray.sort((p1, p2) =>
     p1.questionId > p2.questionId ? 1 : p1.questionId < p2.questionId ? -1 : 0
   );
-  let questions = Object.values(data.questions);
-  let correctOnes = 0;
-  for (let i = 1; i <= questions.length; i++) {
-    const element = questions[i - 1].answers;
-    if (element.correct == arr[i - 1].resId) correctOnes++;
-  }
-  var score = (correctOnes / questions.length) * 100;
-  let feedBack =
-    score < 30 ? "Not Bad" : score < 60 ? "Good results" : "Exellent keep up";
-  $(".container").empty();
-
-  // The every() function behaves exactly like forEach(), except it stops iterating through the array whenever the callback function returns a falsy value.
-  let template = "";
-  let i = 0;
-  questions.forEach((question) => {
-    let options = "";
-    let _class = "";
-    //let test = false;
-    question.options.forEach((option) => {
-      if (
-        holderUserAnswersArray[i].resId == question.answers.correct &&
-        question.answers.correct == option.id
-      ) {
-        _class = "success";
-      } else if (
-        holderUserAnswersArray[i].resId != question.answers.correct &&
-        holderUserAnswersArray[i].resId == option.id
-      )
-        _class = "wrong";
-      else _class = "";
-      options += `<div class="option ${_class}"><div style="display:flex"><span>${option.id}</span>${option.content}</div></div>`;
-    });
-
-    template += `
-        <div>
-        <p>Question ${question.id}/${data.questions.length}</p>
-        <h3 style="padding:30px 0px">${question.question}</h3>
-          ${options}
-          <p class='score_question_comment'><img src='../assets/img/icons8-ok-48.png'>${question.answers.comment}</p>
-        </div>`;
-    i++;
+  var responseJSON = $.ajax({
+    type: "GET",
+    url: "../Models/RequestHandler.php",
+    data: {
+      //questions: JSON.stringify(data),
+      userAnswers: JSON.stringify(arr),
+      quizId: idQuiz,
+    },
+    //dataType: "json",
+    cache: false,
+    async: false,
   });
+  responseJSON = JSON.parse(responseJSON.responseText);
+  console.log(responseJSON);
+  let template = responseJSON.template;
   template += `<button class="btn btn-orange bold" style="width: 100%;margin-top: 100px;padding: 20px;" onclick="loadQuize()">Retake quiz</button>`;
-  // template += `
-  // <button class="btn btn-orange bold" style="width: 100%;height:50px;margin-top:100px" onclick="startQuiz(1)">Retake quiz</button>
-  // `;
-  //document.getElementsByClassName(".container").$(selected).removeAttr('" attribute you want to remove "');
+  $(".container").empty();
   $(".container").removeAttr("style");
   $(".container").css({ color: "white" });
   $("body").css({ height: "fit-content", overflow: "auto" });
   $(".container").html(template);
-  $("#username").text(feedBack + " " + sessionStorage.getItem("name"));
+  $("#username").text(
+    responseJSON.feedback + " " + sessionStorage.getItem("name")
+  );
   document.querySelector(".score").style.opacity = "1";
   document.querySelector(".score").style.visibility = "visible";
-  //$(".score").animate({ display: "block" }, 1000, "swing");
-  $("#score").text(score + "%  Score");
+  $("#score").text(responseJSON.score + "%  Score");
   $("#score_details").html(`
-  You attempt<span style="color:blue"><b> ${questions.length} question</b></span> from that <span style="color:#72d561"><b>${correctOnes} answer </b></span> are correct`);
+  You attempt<span style="color:blue"><b> ${data.length} question</b></span> from that <span style="color:#72d561"><b>${responseJSON.correctOnes} answer </b></span> are correct`);
 }
 
 function random(max, min) {
@@ -266,4 +257,7 @@ function random(max, min) {
     }
   }
   return arr;
+}
+function hideScore() {
+  $(".score").css({ opacity: 0, visibility: "hidden" });
 }
